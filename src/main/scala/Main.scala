b4692d087ab43fa4f8e8ef267a535fc63c5fc154
1994jesseloor <j.loor@hotmail.com>
2020-11-06 10:28:46 +0100

    serialization try out

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,26 +1,58 @@
-class Main {
+import java.io._
 
 
-type Point = Vector[Vector[Float]]
-type Data = Vector[String]
+object Main  extends App {
 
 
-abstract class Tree{
-  def include(tree: Tree): Tree
+  type Point = Array[Double]
+  type Shape = Array[Point]
+  type Pointer = Long
+
+
+  class Node(val left: Pointer, val right: Pointer, val split: Double, val start: Pointer, val end: Pointer) {
+    val length = 5 * 8  // in bytes
+    def serialize: Array[Byte] = {
+      val stream: ByteArrayOutputStream = new ByteArrayOutputStream(length)
+      val oos = new ObjectOutputStream(stream)
+      oos.writeLong(left);
+      oos.writeLong(right);
+      oos.writeDouble(split);
+      oos.writeLong(start);
+      oos.writeLong(end)
+      oos.close()
+      stream.toByteArray()
+    }
+    override def toString = List(left, right, split, start, end).mkString(", ")
+  }
+
+  object Node{
+    def deserialize(bytes: Array[Byte]): Node = {
+      val ois = new ObjectInputStream(new ByteArrayInputStream(bytes))
+      val node = new Node(ois.readLong, ois.readLong, ois.readDouble, ois.readLong, ois.readLong)
+      ois.close()
+      node
+    }
+
+
+  }
+
+
+  val original = new Node(Long.MinValue,Long.MinValue,Double.MinValue,Long.MinValue,Long.MinValue)
+
+  val bytes = original.serialize
+  val inferred = Node.deserialize(bytes)
+  println(inferred)
+
+  println(bytes.length)
+
+  val s = bytes.map(_.toBinaryString).mkString(", ")
+  println(s)
+
+
+  val file = new FileOutputStream("C:\\Users\\Jesse.Loor\\Desktop\\k_d_tree\\bin.bin")
+  file.write(bytes)
+
+
+
 }
 
-class FilledTree(val point: Point, left: Tree, right: Tree, data: Data){
-  def include = new FilledTree(point, left, right, data)
-}
-
-
-
-class VoidTree extends Tree {
-  def include(tree: Tree) = tree
-}
-
-
-
-
-
-}
